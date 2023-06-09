218fe3093828ce64d4c5dcd0b27ce18e421c2740
Francesco Liuzzi <francesco.liuzzi93@gmail.com>
2023-06-09 21:47:17 +0200

    Chiamata Robot profile in body

--- frontend/src/body.jsx
+++ frontend/src/body.jsx
@@ -1,10 +1,20 @@
 import './style/body.css';
 import Map from './components/map';
+import RobotProfile from './components/robotProfile';
 
 function Body() {
   return (
       <div className="main">
-        {/*<img src={map} className="map" alt="map" />*/}
+        <RobotProfile color="white"/>
+        <RobotProfile color="green"/>
+        <RobotProfile color="turquoise"/>
+        <RobotProfile color="black"/>
+        <RobotProfile color="brown"/>
+        <RobotProfile color="yellow"/>
+        <RobotProfile color="orange"/>
+        <RobotProfile color="red"/>
+        <RobotProfile color="pink"/>
+        <RobotProfile color="blue"/>
           <Map />
       </div>
   );
