433f433414e99db5fb1ea1445e5220397db43e4f
Francesco Liuzzi <francesco.liuzzi93@gmail.com>
2023-06-10 01:42:21 +0200

    Inseriti badge per ogni robot

--- frontend/src/components/robotProfile.jsx
+++ frontend/src/components/robotProfile.jsx
@@ -17,59 +17,42 @@ function RobotProfile(props) {
         avatar = greenAvatar;
     }
     else if (props.color === "black") {
-        avatar = blackAvatar;    
-    } 
+        avatar = blackAvatar;
+    }
     else if (props.color === "blue") {
         avatar = blueAvatar;
     }
     else if (props.color === "black") {
-        avatar = blackAvatar;    
+        avatar = blackAvatar;
     }
     else if (props.color === "brown") {
-        avatar = brownAvatar;    
-    } 
+        avatar = brownAvatar;
+    }
     else if (props.color === "orange") {
-        avatar = orangeAvatar;    
-    } 
+        avatar = orangeAvatar;
+    }
     else if (props.color === "pink") {
-        avatar = pinkAvatar;    
-    } 
+        avatar = pinkAvatar;
+    }
     else if (props.color === "red") {
-        avatar = redAvatar;    
-    } 
+        avatar = redAvatar;
+    }
     else if (props.color === "turquoise") {
-        avatar = turquoiseAvatar;    
-    } 
+        avatar = turquoiseAvatar;
+    }
     else if (props.color === "yellow") {
         avatar = yellowAvatar;
-    } 
-    return (
-        <div className="avatar">
-            <img src={avatar} alt={props.color}/>
-        </div>
-        
+    }
 
-    //     <>
-    //         {props.color==="white" ? <div className="avatar"><img src={whiteAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="black" ? <div className="avatar"><img src={blackAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="blue" ? <div className="avatar"><img src={blueAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="brown" ? <div className="avatar"><img src={brownAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="green" ? <div className="avatar"><img src={greenAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="orange" ? <div className="avatar"><img src={orangeAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="pink" ? <div className="avatar"><img src={pinkAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="red" ? <div className="avatar"><img src={redAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="turquoise" ? <div className="avatar"><img src={turquoiseAvatar} alt={props.color}/></div> : null}
-    //         {props.color==="yellow" ? <div className="avatar"><img src={yellowAvatar} alt={props.color}/></div> : null}
-    //    </>
+    return (
+        <div>
+            <div className="avatar">
+                <img src={avatar} alt={props.color} />
+            </div>
+            <div className="borderAvatar">
+                <span>S/N 0000</span>
+            </div>
+        </div>
     );
 }
-
-const SerialNumber = ({ serialNumber }) => {
-    return (
-      <div style={{ border: '1px solid black', width: 'fit-content', maxWidth: '100%' }}>
-        {serialNumber}
-      </div>
-    );
-  };
-
 export default RobotProfile;
