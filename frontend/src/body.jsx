08afe535a55d8b6dbb70908bcaae3dd2722a1d0d
Francesco Liuzzi <francesco.liuzzi93@gmail.com>
2023-06-14 17:27:44 +0200

    Allineamento robot

--- frontend/src/body.jsx
+++ frontend/src/body.jsx
@@ -1,22 +1,28 @@
 import './style/body.css';
 import RobotProfile from './components/robotProfile';
 import Map from './components/map';
+import Stack from '@mui/material/Stack';
+
 
 function Body() {
   return (
-      <div className="main">
-        <RobotProfile color="white"/>
-        <RobotProfile color="green"/>
-        <RobotProfile color="turquoise"/>
-        <RobotProfile color="black"/>
-        <RobotProfile color="brown"/>
-        <Map />
-        <RobotProfile color="yellow"/>
-        <RobotProfile color="orange"/>
-        <RobotProfile color="red"/>
-        <RobotProfile color="pink"/>
-        <RobotProfile color="blue"/>
-      </div>
+    <div className="main">
+      <Stack spacing={5}> 
+        <RobotProfile color="white" />
+        <RobotProfile color="green" />
+        <RobotProfile color="turquoise" />
+        <RobotProfile color="black" />
+        <RobotProfile color="brown" />
+      </Stack>
+      <Map />
+      <Stack spacing={5}>
+        <RobotProfile color="yellow" />
+        <RobotProfile color="orange" />
+        <RobotProfile color="red" />
+        <RobotProfile color="pink" />
+        <RobotProfile color="blue" />
+      </Stack>
+    </div>
   );
 }
 
